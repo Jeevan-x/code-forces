7175fd0d6037000636b639871408677f06bb0b36
Jeevan-x <e.jeevan007@gmail.com>
2026-07-04 11:37:38 +0530

    solved problem 158A

---
+++ 800/X_158A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > 0 && a[i] >= a[k - 1])
+            ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
